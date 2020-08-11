91e9dbb02151b28a4ae41f2bc17e1bfb3b019a8d
Nilton Pimentel <niltonpimentel02@gmail.com>
2020-08-10 23:55:46 -0300

    Terminado 3. Injeção de Dependências
    Close #28

--- libpythonpro_ntg/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro_ntg/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,10 +1,48 @@
-from libpythonpro_ntg.spam.enviador_de_email import Enviador
+from unittest.mock import Mock
+
+import pytest
+
 from libpythonpro_ntg.spam.main import EnviadorDeSpam
+from libpythonpro_ntg.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Nilton', email='niltonpimentel02@gmail.com'),
+            Usuario(nome='Renzo', email='niltonpimentel02@gmail.com')
+        ],
+        [
+            Usuario(nome='Nilton', email='niltonpimentel02@gmail.com')
+        ]
+    ]
+)
+def test_qde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
+        'niltonpimentel02@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
+    assert len(usuarios) == enviador.enviar.call_count
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Nilton', email='niltonpimentel02@gmail.com')
+    sessao.salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'renzo@python.pro.br',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
+    enviador.enviar.assert_called_once_with(
+        'renzo@python.pro.br',
         'niltonpimentel02@gmail.com',
         'Curso Python Pro',
         'Confira os módulos fantásticos'
