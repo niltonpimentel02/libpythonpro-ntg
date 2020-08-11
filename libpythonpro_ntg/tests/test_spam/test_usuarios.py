db314974c66d42eeb1d14407d812d7bdf3d1721c
Nilton Pimentel <niltonpimentel02@gmail.com>
2020-08-10 23:55:46 -0300

    Terminado 3. Injeção de Dependências
    Close #28

--- libpythonpro_ntg/tests/test_spam/test_usuarios.py
+++ libpythonpro_ntg/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libpythonpro_ntg.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Nilton')
+    usuario = Usuario(nome='Nilton', email='niltonpimentel02@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='Nilton'), Usuario(nome='Renzo')]
+    usuarios = [
+        Usuario(nome='Nilton', email='niltonpimentel02@gmail.com'),
+        Usuario(nome='Renzo', email='niltonpimentel02@gmail.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
